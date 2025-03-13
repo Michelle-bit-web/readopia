04d433db1cc891a5fecef77970bafd7fc37d4dba
Michelle <mpuschkarow@web.de>
2025-03-13 19:02:49 +0100

    book main page-simple version

--- script.js
+++ script.js
@@ -394,7 +394,7 @@ let books = [
   //für die Kommentarfunktion
 
   function sendComment(){
-    let name = document.getElementById('username').value;
+    let name = document.getElementById('username').innerHTML;
     let comment = document.getElementById('comment_text').value;
 
     if (name !== "" && comment !== ""){
@@ -405,43 +405,156 @@ let books = [
     comment = "";
   }
 
-  function renderImg(){
+//Bilder Rendern
 
-    let bestSeller = books.filter(best => best.bestseller == true)
-    console.table(bestSeller)
-    for (let i = 0; i < bestSeller.length; i++) {
-    let coverBestSeller = bestSeller[i].cover;
-    document.getElementById(`best_seller${i+1}`).style.backgroundImage = `url('./assets/img/${coverBestSeller}.png')`;
-    }
-  }
-
-
-function switchBestSeller(a){
-  if (a < 0){}
-  if (a > 0){}
+function init(){
+  // renderImg()
+  // renderNewAddition();
+  loadAllBooks();
 }
-// function renderImg(){
- 
-//   for (let indexImg = 0; indexImg < 1; indexImg++) {
-//     let cover = books[indexImg].cover;
-//     document.getElementById('best_seller_render').innerHTML += `<div id="divImg${indexImg}" class="divImg">Hallo</div>`;
-//     getBookImg(indexImg, cover);}
-// }
 
-// function renderImg(){
-//   let bestSeller = document.getElementById('best_seller_render').innerHTML = "";
-//   for (let indexImg = 0; indexImg < books.length; indexImg++) {
-//     let cover = books[indexImg].cover;
-//     bestSeller += `<div id="divImg${indexImg}" class="divImg"></div>`;
-//     getBookImg(indexImg, cover);
+//   function renderImg(){
+
+//     let bestSeller = books.filter(best => best.bestseller == true)
+//     console.table(bestSeller)
+//     for (let i = 0; i < bestSeller.length; i++) {
+//     let coverBestSeller = bestSeller[i].cover;
+//     document.getElementById(`best_seller${i+1}`).style.backgroundImage = `url('./assets/img/${coverBestSeller}.png')`;
+//     currentImg = i;
+//     }
 //   }
+
+//   let currentImg;
+
+// function switchBestSeller(a){
+//   let switchBestSeller = (books.filter(best => best.bestseller == true));
+//   // for (let i = 0; i < switchBestSeller.length; i++) {
+//   //   let coverBestSeller = bestSeller[i].cover;
+//   //   document.getElementById(`best_seller${i+1}`).style.backgroundImage = `url('./assets/img/${coverBestSeller}.png')`;
+//   //   }
+//   if (a < 0 && currentImg == 1){
+//    currentImg =(switchBestSeller.length);
+//   }
+//   else if (a < 0 && currentImg > 1){
+//     currentImg = currentImg + 1;
+//    }
+//   else if (a > 0 && currentImg < (switchBestSeller.length)){
+//     currentImg = currentImg - 1;
+//   }
+//   else if (a > 0 && currentImg == (switchBestSeller.length)){
+//     currentImg = currentImg - (switchBestSeller.length + 1);
+//   }
+// document.getElementById(`best_seller${currentImg}`).style.backgroundImage = `url('./assets/img/${switchBestSeller[currentImg].cover}.png')`;
 // }
 
-// function getBookImg(index, cover){
-//   document.getElementById(`divImg${index}`).style.backgroundImage +=
-//     `url('./assets/img/${cover}.png')'`;
+// function renderNewAddition(){
+//   let newAddition = books.filter(best => best.publishedYear > 2023)
+//     console.table(newAddition)
+//     for (let i = 0; i < newAddition.length; i++) {
+//     let newBooks = newAddition[i].cover;
+//     document.getElementById('div_new_books').innerHTML += `<div id= "newBook${i}" class="new_book">neues Buch x</div>`
+//     document.getElementById(`newBook${i}`).style.backgroundImage = `url('./assets/img/${newBooks}.png')`;
+//     }
+// }
+
+// //Book Page öffnen
+
+// function openOverlay(genre){
+//   if(genre == education){
+//     let image = books.filter(image => image.genre == 'Education');
+//     for (let indexEducation = 0; indexEducation < image.length; indexEducation++) {
+//       getOverlayTemplate(indexEducation);
+//       getOverlayImg(indexEducation, image)
+//       // document.getElementById('education').innerHTML += getOverlayTemplate(indexEducation);
+//       // getOverlayImg(indexEducation, image);
+//     }
+//   }
+//   if(genre == health){
+//     let image = books.filter(image => image.genre == 'Health & Fitness');
+//     for (let indexHealth = 0; indexHealth < image.length; indexHealth++) {
+//       getOverlayTemplate(indexHealth);
+//       getOverlayImg(indexhealth, image)
+//       // document.getElementById('health').innerHTML += getOverlayTemplate(indexHealth);
+//       // getOverlayImg(indexHealth, image);
+//     }
+//   }
+//   if(genre == romance){
+//     let image = books.filter(image => image.genre == 'Romantik');
+//     for (let indexRomance = 0; indexRomance < image.length; indexRomance++) {
+//       getOverlayTemplate(indexRomance);
+//       getOverlayImg(indexRomance, image)
+//       // document.getElementById('romance').innerHTML += getOverlayTemplate(indexRomance);
+//       // getOverlayImg(indexRomance, image);
+//     }
+//   }
+
+//   if(genre == fantasy){
+//     let image = books.filter(image => image.genre == 'Science-Fiction' || image.genre == 'Fantasy');
+//     for (let indexFantasy = 0; indexFantasy < image.length; indexFantasy++) {
+//       getOverlayTemplate(indexFantasy);
+//       getOverlayImg(indexFantasy, image)
+//       // document.getElementById('fantasy').innerHTML += getOverlayTemplate(indexFantasy);
+//       // getOverlayImg(indexFantasy, image);
+//     }
+//   }
+
+// }
+
+// function getOverlayTemplate(index){
+//   let overlay = document.getElementById('overlay');
+//   overlay.classList.remove('d_none');
+//   overlay.innerHTML = `
+//       <div id="overlay_img${index} class="overlay">
+//         <a id="link_bookpage${index}" href="bookPage.html" target="_blank">See more</a>
+//         <button onclick="closeOverlay()">x</button>
+//       <div>
+//       <div id="btn_div" class="btn_overlay_div">
+//          <button "id="btn_left" class="btn" onclick="switchImg(-1, event)"> << </button>
+//          <button "id="btn_right" class="btn" onclick="switchImg(+1, event)" id="btn_right" class="btn"> >> </button>
+//       </div>`;
+//   currentImg = index;
+// }
+
+// function getOverlayImg(index, image){
+//       let book = image[index].cover;
+//       document.getElementById(`edImg${index}`).style.backgroundImage = overlayImg(book)
+// }
+
+// function overlayImg(book){
+//   return `url('./assets/img/${book}.png')`
+// }
+
+// function closeOverlay(){
+//   overlay.classList.add('d_none');
 // }
 
 
-// let book = books.filter(getBookCover => getBookCover.cover == `book${indexImg}`);
-// console.log(book)
+//simplere Variante
+function loadAllBooks(){
+
+  for (let i = 0; i < books.length; i++) {
+    let bookTitles = books[i].name;
+    let bookCovers = books[i].cover;
+    let bookAuthor = books[i].author;
+    let bookDescription = books[i].description;
+    let bookLikes = books[i].likes;
+    let bookPrices = books[i].price;
+    let bookYear = books[i].publishedYear;
+    let bookGenre = books[i].genre;
+    document.getElementById('render_single_books').innerHTML += 
+    renderBookTemplate(i,bookTitles,bookCovers,bookAuthor,bookDescription,bookLikes,bookPrices,bookYear,bookGenre)
+    getCoverImages(bookCovers);
+  }
+}
+
+function getCoverImages(cover){
+  document.getElementById(`${cover}`).style.backgroundImage = `url('./assets/img/${cover}.png')`;
+}
+
+//Vorlage vom Buchstaben-Counter
+function countLetters() {
+  let inputLetters = document.getElementById("inputCounter").value.length;
+  document.getElementById("counter").innerHTML =
+    "<br>Anzahl der Zeichen:" + inputLetters;
+}
+//Ich brauch ja eine Summe aus den Klicks
---
+++ scripts/templates.js
@@ -0,0 +1,34 @@
+function renderBookTemplate(i,title,cover,author,description,likes,price,year,genre){
+    return `<br>
+    <section class="section_book_page">
+        <div class="upper_page_part">
+            <h1>${title}</h1>
+            <div class="div_book_info">
+                <div id="${cover}" class="book_img"></div>
+                <div class="book_informations">
+                <div id="author">Author: ${author}</div>
+                <div id="year">Published: ${year}</div>
+                <div id="genre">Genre: ${genre}</div>
+                <div id="price">Price: ${price}€</div>
+            </div>
+            </div>
+            
+        </div>
+        <div>
+            <div>${likes}<img onclick="LikeOrDislike()" class="heart_icon" src="./assets/icon/heart_icon.png" alt="like button"></div>
+            <p id="book_description">Description:${description}</p>
+        </div>
+    </section>
+    <section class="section_comments">
+        <h2>Give your feedback</h2>
+        <div class="div_comments">
+            <span>user:<span id='username'> Readiculous237</span></span>
+            <label for="comment_text">Your comment:</label>
+            <div class="div_textarea">
+                <textarea name="comment_text" id="comment_text" placeholder="Give your feedback..."></textarea>
+                <img onclick="sendComment()" class="send_icon" src="./assets/icon/send_icon.png" alt="send button">
+            </div>
+        </div>
+        <div id="written_comments" class="written_comments">User comments:</div>
+    </section>`
+}
